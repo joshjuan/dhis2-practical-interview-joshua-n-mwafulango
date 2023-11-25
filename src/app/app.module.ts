9832074c37b84d75e77c4e7668b37af32dd8b4bc
joshjuan <nelsonjoshua301@gmail.com>
2023-11-25 12:42:47 +0300

    COMMENTS ON EACH PART

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 // src/app/app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 
-
-
 @NgModule({
   declarations: [AppComponent, WeatherComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule], // Add FormsModule here
   providers: [],
   bootstrap: [AppComponent],
 })
